ac6e0d11153e4c69c321c92612658e12f9ee4f62
Reza Abbasi <r.abbasi@irantalent.com>
2022-05-24 13:01:43 +0430

    add repository Service providers

---
+++ app/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Providers;
+
+use App\Repositories\Interfaces\MessageRepositoryInterface;
+use App\Repositories\Interfaces\UserRepositoryInterface;
+use App\Repositories\MessageRepository;
+use App\Repositories\UserRepository;
+use Illuminate\Support\ServiceProvider;
+
+class RepositoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind(MessageRepositoryInterface::class, MessageRepository::class);
+        $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
